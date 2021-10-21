a04fc38331e243f2081564c05b3affb5e772e4fc
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-21 23:23:43 +0000

    Adds 7 more tests and code to pass them.

--- launchCodeRocks.js
+++ launchCodeRocks.js
@@ -1,8 +1,22 @@
 function launchOutput(num){
   //your code here
-  if (num % 2 === 0) {
+  if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+    return 'LaunchCode Rocks!';
+  } else if (num % 2 === 0 && num % 3 === 0) {
+    return 'LaunchCode!';
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return 'Code Rocks!';
+  } else if (num % 2 === 0 && num % 5 === 0) {
+    return 'Launch Rocks! (CRASH!!!!)';
+  } else if (num % 2 === 0) {
     return 'Launch';
-  }
+  } else if (num % 3 === 0) {
+    return 'Code';
+  } else if (num % 5 === 0) {
+    return 'Rocks!';
+  } else {
+    return 'Rutabagas! That doesn\'t work.';
+  };
 }
 
 module.exports = launchOutput;
--- spec/launchCodeRocks.spec.js
+++ spec/launchCodeRocks.spec.js
@@ -5,6 +5,34 @@ describe("launchOutput", function(){
   //testing code here...
   it("should return 'Launch' when num is divisible by 2 evenly", function() {
     expect(launchOutput(2)).toEqual('Launch');
-  })
+  });
+
+  it("should return 'Code' when num is divisible by 3 evenly", function() {
+    expect(launchOutput(3)).toEqual('Code');
+  });
+
+  it("should return 'Rocks!' when num is divisible by 5 evenly", function() {
+    expect(launchOutput(5)).toEqual('Rocks!');
+  });
+
+  it("should return 'LaunchCode!' when num is divisible by both 2 and 3 evenly", function() {
+    expect(launchOutput(6)).toEqual('LaunchCode!');
+  });
+
+  it("should return 'Code Rocks!' when num is divisible by both 3 and 5 evenly", function() {
+    expect(launchOutput(15)).toEqual('Code Rocks!');
+  });
+
+  it("should return 'Launch Rocks!' when num is divisible by both 2 and 5 evenly", function() {
+    expect(launchOutput(10)).toEqual('Launch Rocks! (CRASH!!!!)');
+  });
+
+  it("should return 'LaunchCode Rocks!' when num is divisible by 2, 3, and 5 evenly", function() {
+    expect(launchOutput(30)).toEqual('LaunchCode Rocks!');
+  });
+
+  it("should return 'Rutabagas! That doesn't work.' if num isn't divisible by 2, 3 or 5", function() {
+    expect(launchOutput(7)).toEqual('Rutabagas! That doesn\'t work.');
+  });
 
 });
